b2178057935c7cb11f52ad74fa3c36cac8f7023f
zs111122223333 <zs15731276801@163.com>
2022-11-25 15:08:48 +0800

    练习了组件的嵌套

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
-  <div id="app">
+  <div id="app" class="app-container">
     <img alt="Vue logo" src="./assets/logo.png">
-    <h1>欢迎使用Vue</h1>
+    <h1>App 根组件</h1>
+    <hr>
+    <div class="box">
+      <!-- 渲染 Left 组件和 Right 组件 -->
+      <!-- 以标签形式使用注册好的组件 -->
+      <Left></Left>
+      <Right></Right>
+    </div>
   </div>
 </template>
-
+<script>
+//导入需要使用的 .vue组件
+import Left from '@/components/Left.vue'
+import Right from '@/components/Right.vue'
+export default {
+  //注册组件
+  components:{
+    Left,
+    Right
+  }
+}
+</script>
+<style lang="less">
+.app-container{
+  padding: 1px 20px 20px;
+  background-color: #efefef;
+}
+</style>
---
+++ src/components/Left.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="box"></div>
+</template>
+<style lang="less">
+    .box{
+        width: 200px;
+        height: 200px;
+        background-color: pink;
+    }
+</style>
