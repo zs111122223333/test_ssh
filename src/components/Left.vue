9183cfc849af82686879e385afda7eab4e116dc0
zs111122223333 <zs15731276801@163.com>
2022-11-25 15:23:36 +0800

    解决了组件的嵌套的小问题

--- src/components/Left.vue
+++ src/components/Left.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="box"></div>
+    <div class="box1"></div>
 </template>
 <style lang="less">
-    .box{
+    .box1{
+        display: inline-block;
+        margin-left: 0;
         width: 200px;
-        height: 200px;
+        height: 100px;
         background-color: pink;
     }
 </style>
--- src/components/Right.vue
+++ src/components/Right.vue
@@ -1,3 +1,12 @@
 <template>
-    <div>123</div>
+    <div class="box2"></div>
 </template>
+<style lang="less">
+    .box2{
+        display: inline-block;
+        margin-left: 0px;
+        width: 200px;;
+        height: 100px;
+        background-color: blue;
+    }
+</style>
